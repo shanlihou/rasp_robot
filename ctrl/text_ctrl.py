62ba72a15d91245231462362475f2652db54ecdc
shanlihou <shanlihou@gmail.com>
2024-07-22 18:09:30 +0800

    Automated commit

--- ctrl/text_ctrl.py
+++ ctrl/text_ctrl.py
@@ -14,25 +14,26 @@ class TextController(object):
 
         draw = ImageDraw.Draw(image)
 
-        draw.rectangle((0, 0, self.width, self.height), outline=255, fill=0)
+        # draw.rectangle((0, 0, self.width, self.height), outline=255, fill=0)
 
-        text = "Hello World!"
-        draw.text(
-            (off, 0),
-            text,
-            font=self.font,
-            fill=255,
-        )
+        # text = "Hello World!"
+        _ips = self.get_ip_info()
+        if _ips:
+            for i, _ip in enumerate(_ips):
+                draw.text(
+                    (i * 10, 0),
+                    _ip[0],
+                    font=self.font,
+                    fill=255,
+                )
         return image
 
     def get_ip_info(self):
         _pat = re.compile(r'inet\s+([0-9.]+)\s+netmask\s+([0-9.]+)\s+broadcast\s+([0-9.]+)')
         with os.popen('ifconfig') as f:
             _data = f.read()
-            print(_data)
             _find = _pat.findall(_data)
-            print(_find)
-            return _data
+            return _find
 
     def next_frame(self):
         self.get_ip_info()
